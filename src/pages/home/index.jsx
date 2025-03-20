da34e9c3e6b48678a8e6fa48f0a162030c1aaab4
Haran1304 <hari.smiksystems@gmail.com>
2025-03-20 21:18:17 +0530

    footer design

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import Topbar from '../../components/common/Topbar/Topbar'
-import Navbar from '../../components/common/navbar/index'
-import HeroSection from './HeroSection'
-import Clients from './Clients'
+import React from "react";
+import Topbar from "../../components/common/Topbar/Topbar";
+import Navbar from "../../components/common/navbar/index";
+import HeroSection from "./HeroSection";
+import Clients from "./Clients";
+import Footer from "../../components/common/Footer";
 function index() {
-    return (
-        <div className='font-omnesarabic'>
-            <Topbar />
-            <Navbar />
-            <HeroSection />
-            <Clients />
-        </div>
-    )
+  return (
+    <div className="font-omnesarabic">
+      <Topbar />
+      <Navbar />
+      <HeroSection />
+      {/* <Clients /> */}
+      <Footer />
+    </div>
+  );
 }
 
-export default index
+export default index;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,24 @@
 module.exports = {
-    content: [
-      "./src/**/*.{html,js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          omnesarabic: ['OmnesArabic', 'sans-serif'],
-        },
-        colors: {
-          primary: '#B82D97', // Ensure the color is added under 'colors'
-        },
-        backgroundImage: {
-          'custom-gradient': 'linear-gradient(to right, #B82D97, #57B3D3, #76ABFC)',
-        },
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        omnesarabic: ["OmnesArabic", "sans-serif"],
+      },
+      textColor: {
+        primary: "#B82D97",
+        secondary: "#525252",
+        gray800: "#0D062D",
+        // Ensure the color is added under 'colors'
+      },
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(to right, #B82D97, #57B3D3, #76ABFC)",
+      },
+      backgroundColor: {
+        footerprimary: "#F9F9F9", // Replace with your desired color code
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
